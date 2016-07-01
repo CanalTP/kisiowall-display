f462892337d5fe7714a136d8ac0b0a8774224e16
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-07-01 13:15:56 +0200

    New display

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,13 +13,33 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $reposNumber = $this->get('kisiowall.caller.service')->getReposNumber();
+        // $reposNumber = $this->get('kisiowall.caller.service')->getReposNumber();
         $responseTimes = $this->get('kisiowall.caller.service')->getAverageResponseTime();
         $calls = $this->get('kisiowall.caller.service')->getNumberOfCalls();
         $errors = $this->get('kisiowall.caller.service')->getNumberOfErrors();
         $percent = ($errors / $calls) * 100;
         return $this->render('default/index.html.twig', [
-            'reposNumber' => $reposNumber,
+            // 'reposNumber' => $reposNumber,
+            'responseTimes' => json_encode($responseTimes),
+            'calls' => $calls,
+            'errorsPercent' => $percent,
+        ]);
+    }
+    
+    /**
+     * @Route("/home2", name="homepage2")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index2action(Request $request)
+    {
+        // $reposNumber = $this->get('kisiowall.caller.service')->getReposNumber();
+        $responseTimes = $this->get('kisiowall.caller.service')->getAverageResponseTime();
+        $calls = $this->get('kisiowall.caller.service')->getNumberOfCalls();
+        $errors = $this->get('kisiowall.caller.service')->getNumberOfErrors();
+        $percent = ($errors / $calls) * 100;
+        return $this->render('default/index2.html.twig', [
+            // 'reposNumber' => $reposNumber,
             'responseTimes' => json_encode($responseTimes),
             'calls' => $calls,
             'errorsPercent' => $percent,
