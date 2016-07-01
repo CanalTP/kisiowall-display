451f0b094f6813107d3542e522b8b67a5ec2f7f8
Nicolas Berard <nicolas.berard@canaltp.fr>
2016-07-01 16:17:29 +0200

    update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends Controller
     private $kisioWallApiService;
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="Home")
      */
     public function indexAction(Request $request)
     {
@@ -44,7 +44,7 @@ class DefaultController extends Controller
     }
         
     /**
-     * @Route("/tech", name="tech")
+     * @Route("/tech", name="Geekzor")
      */
     public function techAction(Request $request)
     {
@@ -52,57 +52,28 @@ class DefaultController extends Controller
         $beginHours = 8;
         $dayInHours = 10;
         $nbCoffees = 190;
+        $nbPascalHitsDays = 1614;
         $hoursMinutes = intval($now->format('H')) * 60 + intval($now->format('i'));
         
         $this->githubApiService = $this->get('github.caller.service');
         $nbCoffeeRealTime = round(($hoursMinutes - ($beginHours * 60)) * ($nbCoffees / (60 * $dayInHours)));
+        $nbPascalHits = round(($hoursMinutes - ($beginHours * 60)) * ($nbPascalHitsDays / (60 * $dayInHours)));
         $occupiedRooms = $this->get('rooms.caller.service')->getCurrentNbMeetings();
         $reposStats = $this->githubApiService->getReposStats();
         
         return $this->render('default/tech.html.twig', [
             'occupiedRooms' => $occupiedRooms,
             'nbCoffeeRealTime' => $nbCoffeeRealTime,
-            'reposStats' => $reposStats
+            'reposStats' => $reposStats,
+            'nbPascalHits' => $nbPascalHits,
         ]);
     }
-    
+
     /**
-     * @Route("/home2", name="homepage2")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/stephane", name="Stephane")
      */
-    public function index2action(Request $request)
+    public function stephaneAction(Request $request)
     {
-        
-        $this->kisioWallApiService = $this->get('kisiowall.caller.service');
-        
-        $now = new \DateTime();
-        $beginHours = 8;
-        $dayInHours = 10;
-        $nbCoffees = 190;
-        $hoursMinutes = intval($now->format('H')) * 60 + intval($now->format('i'));
-        
-        $nbCoffeeRealTime = round(($hoursMinutes - ($beginHours * 60)) * ($nbCoffees / (60 * $dayInHours)));
-        
-        $responseTimes = $this->kisioWallApiService->getAverageResponseTime();
-        $calls = $this->kisioWallApiService->getNumberOfCalls();
-        $errors = $this->kisioWallApiService->getNumberOfErrors();
-        $totalCalls = $this->kisioWallApiService->getTotalNavitiaCalls();
-        $activeUsers = $this->kisioWallApiService->getActiveUsers();
-        $downloads = $this->kisioWallApiService->getDownloadsByStore();
-        $occupiedRooms = $this->get('rooms.caller.service')->getCurrentNbMeetings();
-        
-        
-        $percent = (1 - $errors / $calls) * 100;
-        return $this->render('default/index2.html.twig', [
-            'nbCoffeeRealTime' => $nbCoffeeRealTime,
-            'responseTimes' => json_encode($responseTimes),
-            'calls' => $calls,
-            'errorsPercent' => $percent,
-            'totalCalls' => $totalCalls,
-            'activeUsers' => $activeUsers,
-            'occupiedRooms' => $occupiedRooms,
-            'downloads' => $downloads,
-        ]);
+        return $this->render('default/stephane.html.twig');
     }
 }
